6fae6891d61697d3945973536db66f26aea6f0ca
bini34 <biniyamambachew@gmail.com>
2023-11-15 18:46:03 +0300

    update header file

--- monty.h
+++ monty.h
@@ -1,13 +1,15 @@
 #ifndef MONTY_H
 #define MONTY_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -15,13 +17,11 @@ typedef struct stack_s
         struct stack_s *prev;
         struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
@@ -29,4 +29,17 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-#endif
+/* Function prototypes */
+void execute_operations(FILE *file, stack_t **stack);
+void push(stack_t **stack, int value);
+void pall(stack_t **stack);
+void free_stack(stack_t *stack);
+void swap(stack_t **stack,unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
