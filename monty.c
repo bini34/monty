3f0b15bc1e8f297f78ead6bd7407f386073d20d6
bini34 <biniyamambachew@gmail.com>
2023-11-17 04:36:36 +0300

    fix betty error

--- monty.c
+++ monty.c
@@ -2,35 +2,32 @@
 
 
 /**
- * process_instruction - Executes non-"push" Monty operations.
+ * process- Executes non-"push" Monty operations.
  * @stack: Double pointer to the stack.
  * @opcode: Operation code.
  * @line_number: Line number being processed.
  *
  * Description: This function executes Monty operations other than "push."
  */
-void process_instruction(stack_t **stack, const char *opcode, unsigned int line_number)
+void process(stack_t **stack, const char *opcode, unsigned int line_number)
 {
-    int i;
-    instruction_t instructions[] = {
-        {"pall", pall}, {"swap", swap}, {"add", add}, {"pchar", pchar},
-        {"sub", sub}, {"div", divide}, {"mul", mul}, {"mod", mod},
-        {"nop", nop}, {NULL, NULL}
-    };
-
-    for (i = 0; instructions[i].opcode != NULL; i++)
-    {
-        if (strcmp(opcode, instructions[i].opcode) == 0)
-        {
-            instructions[i].f(stack, line_number);
-            return;
-        }
-    }
-
-    fprintf(stderr, "L%d: Unknown instruction %s\n", line_number, opcode);
-    exit(EXIT_FAILURE);
+	int i;
+	instruction_t instructions[] = {
+		{"pall", pall}, {"swap", swap}, {"add", add}, {"pchar", pchar},
+		{"sub", sub}, {"div", divide}, {"mul", mul}, {"mod", mod},
+		{"nop", nop}, {NULL, NULL}
+	};
+	for (i = 0; instructions[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+			instructions[i].f(stack, line_number);
+			return;
+		}
+	}
+	fprintf(stderr, "L%d: Unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
 }
-
 /**
  * execute_operations - Parses and executes Monty operations from a file.
  * @file: File pointer to the Monty bytecode file.
@@ -41,31 +38,27 @@ void process_instruction(stack_t **stack, const char *opcode, unsigned int line_
  */
 void execute_operations(FILE *file, stack_t **stack)
 {
-    char opcode[100];
-    int value;
-    unsigned int line_number = 0;
+	char opcode[100];
+	int value;
+	unsigned int line_number = 0;
 
-    while (fscanf(file, "%99s", opcode) == 1)
-    {
-        line_number++;
-
-        if (strcmp(opcode, "push") == 0)
-        {
-            if (fscanf(file, "%d", &value) != 1)
-            {
-                fprintf(stderr, "L%d: Error: usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-            }
-            process_push(stack, value, line_number);
-        }
-        else
-        {
-            process_instruction(stack, opcode, line_number);
-        }
-
-        fscanf(file, "%*[^\n]");
-        fgetc(file);
-    }
+	while (fscanf(file, "%99s", opcode) == 1)
+	{
+		line_number++;
+		if (strcmp(opcode, "push") == 0)
+		{
+			if (fscanf(file, "%d", &value) != 1)
+			{
+				fprintf(stderr, "L%d: Error: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+			process_push(stack, value, line_number);
+		}
+		else
+			process(stack, opcode, line_number);
+		fscanf(file, "%*[^\n]");
+		fgetc(file);
+	}
 }
 
 /**
@@ -79,26 +72,23 @@ void execute_operations(FILE *file, stack_t **stack)
  */
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    stack_t *stack;
+	FILE *file;
+	stack_t *stack;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
-    stack = NULL;
-    execute_operations(file, &stack);
-    fclose(file);
-    free_stack(stack);
-    return EXIT_SUCCESS;
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	stack = NULL;
+	execute_operations(file, &stack);
+	fclose(file);
+	free_stack(stack);
+	return (EXIT_SUCCESS);
 }
-
